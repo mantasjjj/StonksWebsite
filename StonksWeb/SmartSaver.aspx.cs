b588c4063a264a563b3b43ae2e0f0183090e53ae
Kometta <monika@tsociety.io>
2020-11-16 00:15:14 +0200

    Sliders representing data

--- StonksWeb/SmartSaver.aspx.cs
+++ StonksWeb/SmartSaver.aspx.cs
@@ -13,6 +13,23 @@ namespace StonksWeb
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            var HousingSlider = Housing;
+            var GroceriesSlider = Housing;
+            var TransportationSlider = Transportation;
+            var EntertainmentSlider = Entertainment;
+            var HealthSlider = Health;
+            var ShoppingSlider = Shopping;
+            var UtilitiesSlider = Utilities;
+            var OtherSlider = Other;
+
+            HousingSlider.Value = FinancialPlanController.ActivePlan.GetExpense(ExpenseType.Housing).Value.ToString();
+            GroceriesSlider.Value = FinancialPlanController.ActivePlan.GetExpense(ExpenseType.Groceries).Value.ToString();
+            TransportationSlider.Value = FinancialPlanController.ActivePlan.GetExpense(ExpenseType.Transportation).Value.ToString();
+            EntertainmentSlider.Value = FinancialPlanController.ActivePlan.GetExpense(ExpenseType.Entertainment).Value.ToString();
+            HealthSlider.Value = FinancialPlanController.ActivePlan.GetExpense(ExpenseType.Health).Value.ToString();
+            ShoppingSlider.Value = FinancialPlanController.ActivePlan.GetExpense(ExpenseType.Shopping).Value.ToString();
+            UtilitiesSlider.Value = FinancialPlanController.ActivePlan.GetExpense(ExpenseType.Utilities).Value.ToString();
+            OtherSlider.Value = FinancialPlanController.ActivePlan.GetExpense(ExpenseType.Other).Value.ToString();
         }
 
         protected void Page_Init(object sender, EventArgs e)
@@ -28,6 +45,7 @@ namespace StonksWeb
                 { ExpenseType.Utilities, TextBoxUtilities },
                 { ExpenseType.Other, TextBoxOther }
             };
+
         }
         protected void Page_LoadComplete(object sender, EventArgs e)
         {
@@ -40,6 +58,12 @@ namespace StonksWeb
                 }
             }
         }
+
+
+        protected void SetRangeSlider()
+        {
+        }
+
 
     }
 }
